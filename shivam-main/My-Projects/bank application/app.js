d5d75169db7ded3c75090890516dd0aa9a77b850
shivaarje333 <shivaarje20@gmail.com>
2023-01-24 09:44:26 +0530

    added file

---
+++ shivam-main/My-Projects/bank application/app.js
@@ -0,0 +1,47 @@
+let signup = document.getElementById('signup');
+let login = document. getElementById('login');
+let signbtn = document.getElementById('signButton');
+let loginbtn = document.getElementById('loginbtn');
+
+//access ragster email or pass
+let ragisterEmail = document.getElementById('email');
+let ragisterPass = document.getElementById('pass');
+
+//access login email or password
+let loginEmail = document.getElementById('logEmail');
+let loginPass = document.getElementById('logPass');
+
+
+
+let array = [];
+
+function addData(){
+    if(ragisterEmail.value == ''){
+        alert('please fill email');
+    }
+    else if(ragisterPass.value == ''){
+        alert('please fill password');
+    }
+    else{
+        console.log('successfull fill');
+        signup.style.display = "none";
+        login.style.display = "block";
+        array.push({email : ragisterEmail.value, pass : ragisterPass.value});
+         console.log(array);
+    }
+}
+
+function matchUser(){
+    if(loginEmail.value == ''){
+        alert('please fill valid email');
+    }
+    else if(loginPass.value == ''){
+        alert('please fill valid password');
+    }
+    else{
+        login.style.display = "block";
+        signup.style.display = "block";
+    }
+}
+
+   
